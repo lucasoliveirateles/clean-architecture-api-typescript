f11ef1d86839b78f35f3cad47748329a8a3eae56
Lucas Teles <dev.teles@outlook.com>
2021-09-19 03:15:48 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -17,11 +17,22 @@ describe('Jwt Adapter', () => {
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
-  test('Should call sign with correct values', async () => {
+  test('Should return a token on sign success', async () => {
     const sut = new JwtAdapter('secret')
 
     const accessToken = await sut.encrypt('any_id')
 
     expect(accessToken).toBe('any_token')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_id')
+
+    await expect(promise).rejects.toThrow()
   })
 })
