1ed11a01a39003b6a5dfc6c9bbec87422101d500
Lucas Teles <dev.teles@outlook.com>
2021-09-30 09:13:08 -0300

    test: ensure DbLoadAccountByToken returns an account success

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -78,14 +78,11 @@ describe('DbLoadAccountByToken Usecase', () => {
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role')
   })
 
-  test('Should return null if LoadAccountByTokenRepository returns null', async () => {
-    const { sut, loadAccountByTokenRepository } = makeSut()
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
 
-    jest.spyOn(loadAccountByTokenRepository, 'loadByToken').mockReturnValueOnce(
-      new Promise(resolve => resolve(null))
-    )
     const account = await sut.load('any_token', 'any_role')
 
-    expect(account).toBeNull()
+    expect(account).toEqual(makeFakeAccount())
   })
 })
