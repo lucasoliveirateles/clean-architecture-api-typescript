e713377396e02c9aa78412fd14d47de25fc89fc2
Lucas Teles <dev.teles@outlook.com>
2021-10-01 20:21:51 -0300

    test: ensure JwtAdapter returns a decrypted value on verify

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -47,13 +47,21 @@ describe('Jwt Adapter', () => {
   })
 
   describe('verify()', () => {
-    test('Should call veify with correct values', async () => {
+    test('Should call verify with correct values', async () => {
       const sut = makeSut()
       const signSpy = jest.spyOn(jwt, 'verify')
 
       await sut.decrypt('any_token')
 
       expect(signSpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+
+    test('Should return a value on verify success', async () => {
+      const sut = makeSut()
+
+      const value = await sut.decrypt('any_token')
+
+      expect(value).toBe('any_value')
     })
   })
 })
