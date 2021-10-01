c90627c00d3ce9676bf522b67e4efb5464ecc9cb
Lucas Teles <dev.teles@outlook.com>
2021-10-01 20:24:39 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -63,5 +63,16 @@ describe('Jwt Adapter', () => {
 
       expect(value).toBe('any_value')
     })
+
+    test('Should throw if verify throws', async () => {
+      const sut = new JwtAdapter('secret')
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+      const promise = sut.decrypt('any_token')
+
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
