c46ad16df786aca1d4d058d4935fb581e64ca6cd
Lucas Teles <dev.teles@outlook.com>
2021-10-19 15:55:00 -0300

    test: ensure DbLoadSurveys return a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -59,4 +59,12 @@ describe('DbLoadSurveys', () => {
 
     expect(loadAllSpy).toHaveBeenCalled()
   })
+
+  test('Should return a list of Surveys on success', async () => {
+    const { sut } = makeSut()
+
+    const surveys = await sut.load()
+
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
