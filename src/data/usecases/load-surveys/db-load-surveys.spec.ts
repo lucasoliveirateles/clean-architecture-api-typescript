87f982380cc7aec9c017ee711aa92743cd5bc765
Lucas Teles <dev.teles@outlook.com>
2021-10-26 15:49:08 -0300

    refactor: rename test suit

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -68,7 +68,7 @@ describe('DbLoadSurveys', () => {
     expect(surveys).toEqual(makeFakeSurveys())
   })
 
-  test('Should throw if LoadsurveysRepository throws', async () => {
+  test('Should throw if LoadSurveysRepository throws', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut()
 
     jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(
