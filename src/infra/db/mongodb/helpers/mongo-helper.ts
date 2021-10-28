ed390e79272c712599d8ddaa0db9bc32c6aefc1d
Lucas Teles <dev.teles@outlook.com>
2021-10-28 07:15:45 -0300

    feat: ensure SurveyResultMongoRepository insert surveyResult if there is no data

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
 import { Collection, MongoClient } from 'mongodb'
-import { AccountModel } from '~/domain/models/account'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -27,7 +26,7 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  map: (collection: any): AccountModel => {
+  map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
 
     return Object.assign({}, collectionWithoutId, { id: _id })
---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,27 @@
+import {
+  SaveSurveyResultRepository
+} from '~/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { SurveyResultModel } from '~/domain/models/survey-result'
+import { SaveSurveyResultModel } from '~/domain/usecases/save-survey-result'
+import { MongoHelper } from '~/infra/db/mongodb/helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+
+    const response = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+
+    return response.value && MongoHelper.map(response.value)
+  }
+}
