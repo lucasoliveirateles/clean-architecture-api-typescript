ee36a5ab86dc578118182e0794371a9efe7605c7
Lucas Teles <dev.teles@outlook.com>
2021-10-29 02:58:25 -0300

    test: ensure SaveSurveyResultController returns 500 if SaveSurveyResult throws

--- src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -154,6 +154,18 @@ describe('SaveSurveyResultControoler', () => {
     })
   })
 
+  test('Should return 500 if SaveSurveyResult throws', async () => {
+    const { sut, saveSurveyResultStub } = makeSut()
+
+    jest.spyOn(saveSurveyResultStub, 'save').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
   test('Should return 200 on success', async () => {
     const { sut } = makeSut()
 
