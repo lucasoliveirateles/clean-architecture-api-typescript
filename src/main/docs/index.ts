64867546aaa378311a16f642fc98f5aa6d1c26ff
Lucas Teles <dev.teles@outlook.com>
2021-11-17 23:01:51 -0300

    docs: add load-signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,4 +1,4 @@
-import { loginPath, surveyPath } from './paths'
+import { loginPath, surveyPath, signUpPath } from './paths'
 import {
   accountSchema,
   errorSchema,
@@ -6,7 +6,8 @@ import {
   surveySchema,
   surveysSchema,
   surveyAnswerSchema,
-  apiKeyAuthSchema
+  apiKeyAuthSchema,
+  signUpParamsSchema
 } from './schemas'
 import {
   badRequest,
@@ -37,11 +38,13 @@ export default {
   }],
   paths: {
     '/login': loginPath,
+    '/signup': signUpPath,
     '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
+    signUpParams: signUpParamsSchema,
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
