8ec14eaa43e85da4a64035cf7882a31c8b6f40b8
Lucas Teles <dev.teles@outlook.com>
2021-12-02 06:34:37 -0300

    feat: ensure DbLoadSurveyResult return a surveyResult on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -8,8 +8,8 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (private readonly loadSurveyResultRespository: LoadSurveyResultRepository) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
-    await this.loadSurveyResultRespository.loadBySurveyId(surveyId)
+    const surveyResult = await this.loadSurveyResultRespository.loadBySurveyId(surveyId)
 
-    return null
+    return surveyResult
   }
 }
