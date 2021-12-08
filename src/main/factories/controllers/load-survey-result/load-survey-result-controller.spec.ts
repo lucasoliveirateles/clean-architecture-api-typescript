baf5051e93594b3ea1ffcbffcad407923985783c
Lucas Teles <dev.teles@outlook.com>
2021-12-08 09:12:27 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/main/factories/controllers/load-survey-result/load-survey-result-controller.spec.ts
+++ src/main/factories/controllers/load-survey-result/load-survey-result-controller.spec.ts
@@ -2,9 +2,10 @@
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import {
   HttpRequest,
-  LoadSurveyById
+  LoadSurveyById,
+  LoadSurveyResult
 } from './load-survey-result-controller-protocols'
-import { mockLoadSurveyById } from '~/presentation/test'
+import { mockLoadSurveyById, mockLoadSurveyResult } from '~/presentation/test'
 import { forbidden, serverError } from '~/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '~/presentation/errors'
 import { throwError } from '~/domain/test'
@@ -18,14 +19,22 @@ const makeFakeRequest = (): HttpRequest => ({
 type SutTypes = {
   sut: LoadSurveyResultController
   loadSurveyByIdStub: LoadSurveyById
+  loadSurveyResultStub: LoadSurveyResult
 }
 
 const makeSut = (): SutTypes => {
   const loadSurveyByIdStub = mockLoadSurveyById()
-  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+  const loadSurveyResultStub = mockLoadSurveyResult()
+
+  const sut = new LoadSurveyResultController(
+    loadSurveyByIdStub,
+    loadSurveyResultStub
+  )
+
   return {
     sut,
-    loadSurveyByIdStub
+    loadSurveyByIdStub,
+    loadSurveyResultStub
   }
 }
 
@@ -60,5 +69,15 @@ describe('LoadSurveyResult Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should call LoadSurveyResult with correct value', async () => {
+    const { sut, loadSurveyResultStub } = makeSut()
+
+    const loadSpy = jest.spyOn(loadSurveyResultStub, 'load')
+
+    await sut.handle(makeFakeRequest())
+
+    expect(loadSpy).toHaveBeenCalledWith('any_id')
   })
 })
