20e7eaaf24ddd8367811415b3aa25dd865e4327c
Lucas Teles <dev.teles@outlook.com>
2021-12-08 09:11:44 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -2,6 +2,9 @@ import {
   SaveSurveyResult, SaveSurveyResultParams
 } from '~/domain/usecases/survey-result/save-survey-result'
 import { SurveyResultModel } from '~/domain/models/survey-result'
+import {
+  LoadSurveyResult
+} from '~/domain/usecases/survey-result/load-survey-result'
 import { mockSurveyResultModel } from '~/domain/test'
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
@@ -12,4 +15,14 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
   }
 
   return new SaveSurveyResultStub()
+}
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+  class LoadSurveyResultStub {
+    async load (surveyId: string): Promise<SurveyResultModel> {
+      return await Promise.resolve(mockSurveyResultModel())
+    }
+  }
+
+  return new LoadSurveyResultStub()
 }
