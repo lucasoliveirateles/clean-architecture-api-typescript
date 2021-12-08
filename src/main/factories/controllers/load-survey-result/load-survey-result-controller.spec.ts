b24b04e8bd554ce711e9dfb6f240d0b9cd3faac4
Lucas Teles <dev.teles@outlook.com>
2021-12-08 09:23:25 -0300

    test: ensure LoadSurveyResultController return 200 on success

--- src/main/factories/controllers/load-survey-result/load-survey-result-controller.spec.ts
+++ src/main/factories/controllers/load-survey-result/load-survey-result-controller.spec.ts
@@ -6,9 +6,14 @@ import {
   LoadSurveyResult
 } from './load-survey-result-controller-protocols'
 import { mockLoadSurveyById, mockLoadSurveyResult } from '~/presentation/test'
-import { forbidden, serverError } from '~/presentation/helpers/http/http-helper'
+import {
+  forbidden,
+  ok,
+  serverError
+} from '~/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '~/presentation/errors'
-import { throwError } from '~/domain/test'
+import { mockSurveyResultModel, throwError } from '~/domain/test'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   params: {
@@ -39,6 +44,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('Should call LoadSurveyById with correct value', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
 
@@ -90,5 +103,13 @@ describe('LoadSurveyResult Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(ok(mockSurveyResultModel()))
   })
 })
